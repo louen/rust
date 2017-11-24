0d47ebf26c64c61c3ee7809ff13c453f00b32bc7
Valentin Roussellet <valentin.roussellet@irit.fr>
2017-11-24 15:40:28 +0100

    Create hello world project

---
+++ helloworld/main.rs
@@ -0,0 +1,7 @@
+// This is a comment
+/// This is a doc comment
+
+fn main() {                     // Defines a function
+    println!("Hello,World");    // ! means it is a macro (=metaprogram)
+}
+
